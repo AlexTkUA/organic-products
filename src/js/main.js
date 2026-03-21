534eff58ef1393c91b1c8ead928d0646805a67b4
Alex Tkachenko <sasha.tkachenko1699@gmail.com>
2026-03-21 16:14:49 +0200

    before swiper02

--- src/js/main.js
+++ src/js/main.js
@@ -15,63 +15,10 @@ document.addEventListener("DOMContentLoaded", () => {
     /*===========================================*/
     
     /**=============Product list on main page=========== */
-    const products = [
-  {
-    category: "Vegetable",
-    name: "Calabrese Broccoli",
-    price: "$20.00",
-    discountPrice: "$13.00",
-    rating: 5,
-    isDiscount: true,
-    scr: "product01.png"
-  },
-  {
-    category: "Fresh",
-    name: "Fresh Banana Fruites",
-    price: "$18.00",
-    discountPrice: "$14.00",
-    rating: 4,
-    isDiscount: true,
-    scr: "product02.png"
-  },
-  {
-    category: "Millets",
-    name: "White Nuts",
-    price: "$15.00",
-    discountPrice: "$15.00",
-    rating: 4,
-    isDiscount: false,
-    scr: "product03.png"
-  },
-  {
-    category: "Vegetable",
-    name: "Vegan Red Tomato",
-    price: "$17.00",
-    discountPrice: "$12.00",
-    rating: 5,
-    isDiscount: true,
-    scr: "product04.png"
-  },
-  {
-    category: "Health",
-    name: "Mung Bean",
-    price: "$11.00",
-    discountPrice: "$11.00",
-    rating: 3,
-    isDiscount: false,
-    scr: "product05.png"
-  },
-  {
-    category: "Nuts",
-    name: "Brown Hazelnut",
-    price: "$25.00",
-    discountPrice: "$18.00",
-    rating: 5,
-    isDiscount: true,
-    scr: "product06.png"
-  }
-];
 getData("src/data/products.json", true, "[data-loader]").then(data => {
   renderList(data, ".shop_list")
+  let vegetables = data.filter(item => item.category === "Vegetable");
+  vegetables = vegetables.slice(0,4);
+  renderList(vegetables, ".bestProduct_wrapper")
 })
 })
